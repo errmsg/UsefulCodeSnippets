6ea45453a9587aeb985041857aea101d5460bdf0
errmsg <hfuatalsan@gmail.com>
2016-02-13 00:44:05 +0200

    -Initial commit -Not much right now but eventually this repo will be populated

---
+++ README.md
@@ -0,0 +1,5 @@
+# Useful Code Snippets
+I will keep my snippets here for later use. 
+
+## License
+MIT
---
+++ cpp.hpp
@@ -0,0 +1,3 @@
+// Calculates the size of a C-style array
+int a[5];
+std::cout << "Size: " << sizeof(a)/sizeof(*a) << std::endl;
