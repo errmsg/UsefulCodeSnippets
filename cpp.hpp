174e1dac7c8af1b24d21ba07ac31961eb0cac5f4
errmsg <hfuatalsan@gmail.com>
2016-02-13 17:03:05 +0200

    -Fast sigmoid is added

--- cpp.hpp
+++ cpp.hpp
@@ -1,3 +1,8 @@
 // Calculates the size of a C-style array
 int a[5];
 std::cout << "Size: " << sizeof(a)/sizeof(*a) << std::endl;
+
+// Fast sigmoid f(x) = x / (1 + abs(x))
+double fastSigmoid(double in){
+  return in / (1.0 + (in < 0.0 ? -in : in));
+}
